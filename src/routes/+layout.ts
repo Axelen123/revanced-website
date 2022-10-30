80d5388ad0c1f4bf1ff2af1d2e9210f4bb40123e
Ax333l <main@axelen.xyz>
2022-10-30 20:29:27 +0100

    remove old commented stuff

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,4 +1,3 @@
-// import type { Repository } from 'src/data/types';
 import type { PageLoad } from './$types';
 
 import { contributors } from '../data/api';
@@ -6,11 +5,3 @@ import { contributors } from '../data/api';
 export const prerender = true;
 
 export const load: PageLoad = contributors.page_load_impl();
-
-// export async function load({
-//   fetch
-// }): Promise<ContribData> {
-// 	const response = await fetch(api_url('contributors'));
-// 	const data = await response.json();
-// 	return data;
-// };
--- src/routes/patches/+page.ts
+++ src/routes/patches/+page.ts
@@ -1,28 +1,3 @@
-// import type { Patch } from 'src/data/types';
-
-// import { api_url } from '$lib/utils';
-
-// import { readable } from 'svelte/store';
-
-// export async function load({
-//   fetch
-// }): Promise<PatchesData> {
-// 	const response = await fetch(api_url('patches'));
-// 	const patches = await response.json();
-// 	let packages: string[] = [];
-
-// 	// gets packages
-// 	for (let i = 0; i < patches.length; i++) {
-// 		patches[i].compatiblePackages.forEach((pkg: Patch) => {
-// 			let index = packages.findIndex((x) => x == pkg.name);
-// 			if (index === -1) {
-// 				packages.push(pkg.name);
-// 			}
-// 		});
-// 	}
-// 	return { patches, packages };
-// };
-
 import type { PageLoad } from './$types';
 
 import { patches } from '../../data/api';
